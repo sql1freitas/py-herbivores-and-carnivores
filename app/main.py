dd18740e414051325a006ef5f88535b9c0cf799c
Mathes Freitas <sql1freitas@gmail.com>
2025-04-08 15:01:27 -0300

    Realização do exercício

--- app/main.py
+++ app/main.py
@@ -1,1 +1,33 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name, health=100):
+        self.name = name
+        self.health = health
+        self.hidden = False
+        Animal.alive.append(self)
+        self._check_alive()
+
+    def __repr__(self):
+        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+
+    def _check_alive(self):
+        if self.health <= 0 and self in Animal.alive:
+            Animal.alive.remove(self)
+
+
+class Herbivore(Animal):
+    def hide(self):
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, other):
+        if not isinstance(other, Herbivore):
+            return
+        if other.hidden:
+            return
+        other.health -= 50
+        if other.health <= 0:
+            other.health = 0
+        other._check_alive()
